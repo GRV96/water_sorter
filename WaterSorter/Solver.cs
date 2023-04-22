7b3ae304dc594e02c236541e24616500434da160
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2023-04-22 16:48:38 -0400

    Class Move moved to a separate file

--- WaterSorter/Solver.cs
+++ WaterSorter/Solver.cs
@@ -2,48 +2,6 @@
 
 namespace WaterSorter
 {
-    class Move
-    {
-        public int FromIndex { get; init; }
-        public int ToIndex { get; init; }
-
-        public Move(int fromIndex, int toIndex)
-        {
-            FromIndex = fromIndex;
-            ToIndex = toIndex;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if(obj == null)
-            {
-                return false;
-            }
-
-            Move other = null;
-            if(obj.GetType() == typeof(Move))
-            {
-                other = (Move) obj;
-            }
-            else
-            {
-                return false;
-            }
-
-            return this.FromIndex == other.FromIndex && this.ToIndex == other.ToIndex;
-        }
-
-        public bool IsReverseOf(Move other)
-        {
-            return this.FromIndex == other.ToIndex && this.ToIndex == other.FromIndex;
-        }
-
-        public override string ToString()
-        {
-            return $"{FromIndex} -> {ToIndex}";
-        }
-    }
-
     class Solver
     {
         private List<Stack<string>> bottles = null;
